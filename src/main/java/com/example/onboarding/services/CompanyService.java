eab2decb26d6af5cf8917ebb1b288f72175de93b
gian.uy <gian.uy@finstro.com>
2022-08-08 12:23:39 +0800

    Fix US country name, add component scan bean, add profile annotation

--- src/main/java/com/example/onboarding/services/CompanyService.java
+++ src/main/java/com/example/onboarding/services/CompanyService.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile({"default", "australia", "united-states", "philippines"})
+@Profile({"default", "australia", "unitedstates", "philippines"})
 public class CompanyService {
 
     @Autowired
